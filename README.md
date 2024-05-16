379e565bc5d85d3250ec5b82928b68f13e2599c1
Bhanu <bhanu.galo@gmail.com>
2024-05-16 13:04:08 +0530

    NEW

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# QCM
+# Maintenance
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/attendance_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/attendance_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gautam.qcm
+package com.gautamsolar.maintenance
 
 import io.flutter.embedding.android.FlutterActivity
 
